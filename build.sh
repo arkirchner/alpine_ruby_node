ffdebf4cd6ae16a5f85505bcaf8907ba98a5a6f6
Armin Kirchner <post.armin@gmail.com>
2019-02-06 17:10:31 +0900

    Docker files for each ruby version

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
-docker build . -t akirchner/alpine_ruby_node:2.5.1 --no-cache
-docker push akirchner/alpine_ruby_node:2.5.1
+# Not working because of musl c stack level to deep
+#
+# # Ruby 2.4.4
+# docker build -f Dockerfile.2.4.4 . -t akirchner/alpine_ruby_node:2.4.4 --no-cache
+# docker push akirchner/alpine_ruby_node:2.4.4
+#
+#
+# # Ruby 2.5.0
+# docker build -f Dockerfile.2.5.0 . -t akirchner/alpine_ruby_node:2.5.0 --no-cache
+# docker push akirchner/alpine_ruby_node:2.5.0
+#
+# # Ruby 2.5.1
+# docker build -f Dockerfile.2.5.1 . -t akirchner/alpine_ruby_node:2.5.1 --no-cache
+# docker push akirchner/alpine_ruby_node:2.5.1
